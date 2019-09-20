a303733f05d23a939fb3666bc1580d3ae1503d55
Petrik Lajos Szg <info@petrik.hu>
2019-09-20 11:42:37 +0200

    Consolera kiiras

--- 14S_01/Program.cs
+++ 14S_01/Program.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace _14S_01 {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("My first git project!");
+            Console.WriteLine("Press enter to EXIT!");
+            Console.ReadLine();
         }
     }
 }
